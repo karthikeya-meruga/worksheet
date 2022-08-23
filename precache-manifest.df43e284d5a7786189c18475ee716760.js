08114946310781c1defe4894d777de3345207fd8
karthikeya-meruga <71162101+karthikeya-meruga@users.noreply.github.com>
2022-08-24 02:05:02 +0530

    init

---
+++ precache-manifest.df43e284d5a7786189c18475ee716760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4733e4fe8f0548ba60e79f4724e22e37",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78d4a0ca5818437e15d7",
+    "url": "./static/css/main.aae1e292.chunk.css"
+  },
+  {
+    "revision": "685aa21387c064aef5d3",
+    "url": "./static/js/2.12c8d164.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.12c8d164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d4a0ca5818437e15d7",
+    "url": "./static/js/main.43f49bb6.chunk.js"
+  },
+  {
+    "revision": "812dc62e2b71769542df",
+    "url": "./static/js/runtime-main.1d929811.js"
+  }
+]);
